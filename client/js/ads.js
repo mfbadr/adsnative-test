8b5f035d1477c05a083fc4312a12c66a4a6ff77a
Mikey Badr <mfbadr@gmail.com>
2015-02-22 14:13:30 -0500

    fiddling with AN code

---
+++ client/js/ads.js
@@ -0,0 +1,17 @@
+/* jshint camelcase:false*/
+/* global AdsNative */
+'use strict';
+
+var infeed_placement = new AdsNative('PLACEMENT-KEY', ['travel', 'food']);
+infeed_placement.fetchAd(function(status){
+  if(status){ // If returned a valid campaign to display.
+    $(document).ready(function(){
+      var didDisplay = infeed_placement.displayAd('adgoeshere');
+      if(!didDisplay){
+        console.log('Ad could not be displayed. Most likely due to invalid element ID or double rendering of ad.');
+      }
+    });
+  }
+});
+
+console.log('ads.js', infeed_placement);
